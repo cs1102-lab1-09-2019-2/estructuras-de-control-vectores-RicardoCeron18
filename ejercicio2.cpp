388a6659c2ea6d779942e3bd96a3189a2847c9a5
RicardoCeron18 <54275916+RicardoCeron18@users.noreply.github.com>
2019-08-29 16:41:26 -0500

    EJERCICIO 2
    EJERCICIO 2 RESUELTO

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,17 +1,42 @@
 
 
+#include <vector>
+#include <iostream>
+using namespace std;
 int moda(vector<int> &lista);
 vector<int> leerConsole();
 
 vector<int> leerConsola() {
     //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
+    vector<int> elementos;
+    int numero;
+    do {
+        cout << "Ingrese un entero positivo, escriba -1 si ya no quiere ingresar mas numeros:";
+        cin >> numero;
+        elementos.insert(elementos.end(), numero);
+
+    } while (numero != -1);
+    return elementos;
 }
 
 int moda(vector<int> &lista) {
     /*
     La moda es el elemento que mas se repite en la lista de elementos
      */
-    return null;
+    int mayor,indicemoda;
+    for(int i=0 ; i<lista.size() ; i++){
+        int numerorepeticiones=0;
+        for(int j=0 ; j<lista.size() ; j++){
+            if(lista[i]==lista[j] && i!=j){
+                numerorepeticiones++;
+            }
+        }
+        if(numerorepeticiones>= mayor){
+            mayor=numerorepeticiones;
+            indicemoda=i;
+        }
+    }
+    return lista[indicemoda];
 }
 
 
